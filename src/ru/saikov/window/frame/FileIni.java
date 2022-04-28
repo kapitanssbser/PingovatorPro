5268b7a2345401c28459e8e0c317d9cd5d9e7a85
kapitanssbser <chasovshik.market@yandex.ru>
2022-04-28 22:12:59 +0300

    Реализован класс FileIni.

---
+++ src/ru/saikov/window/frame/FileIni.java
@@ -0,0 +1,59 @@
+package ru.saikov.window.frame;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileIni {
+    ArrayList<String> adress;   //IP адрес лист
+    int adresCount;  //Количество адресов
+    int timeOut;     //Таймаут
+    int time;        //Задержка опроса
+    String pathToFileIni;  //Путь и имя файла настроек
+
+    public FileIni(String a_pathToFile){
+        this.pathToFileIni = a_pathToFile;
+        //Заполняем данными все поля
+        try {
+            FileReader fileReader = new FileReader(new File(a_pathToFile));
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                //System.out.println(line);
+                if (line.equals("[ADRESS COUNT]")){
+                    //String s = reader.readLine();
+                    //System.out.println(s);
+                    this.adresCount = Integer.parseInt(reader.readLine());
+                }
+                if (line.equals("[ADRESS]")){
+                    //String s;
+                    ArrayList<String> myList = new ArrayList<String>();
+                    //System.out.println("Найдено");
+                    for(int i = 0; i < this.adresCount; i++){
+                        //s = reader.readLine();
+                        //System.out.println(s);
+                        //this.adress.add(s);
+                        //myList = new ArrayList<String>();
+                        myList.add(reader.readLine());
+                        this.adress = myList;
+                    }
+                }
+                if (line.equals("[TIMEOUT]")){
+                    this.timeOut = Integer.parseInt(reader.readLine());
+                }
+                if (line.equals("[TIME]")){
+                    this.time = Integer.parseInt(reader.readLine());
+                }
+
+            }
+            reader.close();
+            //Знаем сколько адресов
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+
+}
--- src/ru/saikov/window/frame/Main.java
+++ src/ru/saikov/window/frame/Main.java
@@ -1,6 +1,7 @@
 package ru.saikov.window.frame;
 
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -12,43 +13,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        for (int i = 0; i < 100;i++) {
-            try {
-                InetAddress address1 = InetAddress.getByName(SERVER1); //IP который мне дал Василий
-                boolean reachableServer1 = address1.isReachable(10000);
-                InetAddress address2 = InetAddress.getByName(SERVER2); //IP который мне дал Василий
-                boolean reachableServer2 = address2.isReachable(10000);
-                InetAddress address3 = InetAddress.getByName(SERVER3); //IP который мне дал Василий
-                boolean reachableServer3 = address3.isReachable(10000);
-                InetAddress address4 = InetAddress.getByName(SERVER4); //IP который мне дал Василий
-                boolean reachableServer4 = address4.isReachable(10000);
+    //    for (int i = 0; i < 100;i++) {
+    //        try {
+    //            InetAddress address1 = InetAddress.getByName(SERVER1); //IP который мне дал Василий
+    //            boolean reachableServer1 = address1.isReachable(10000);
+    //            InetAddress address2 = InetAddress.getByName(SERVER2); //IP который мне дал Василий
+    //            boolean reachableServer2 = address2.isReachable(10000);
+    //            InetAddress address3 = InetAddress.getByName(SERVER3); //IP который мне дал Василий
+    //            boolean reachableServer3 = address3.isReachable(10000);
+    //            InetAddress address4 = InetAddress.getByName(SERVER4); //IP который мне дал Василий
+    //            boolean reachableServer4 = address4.isReachable(10000);
 
-                if (reachableServer1) {
-                    System.out.println("Запрос :" + i + " результат Server1 : Пингуется!");
-                } else {
-                    System.out.println("Запрос :" + i + "результат Server1 : Не пингуется!");
-                }
-                if (reachableServer2) {
-                    System.out.println("Запрос :" + i + " результат Server2 : Пингуется!");
-                } else {
-                    System.out.println("Запрос :" + i + "результат Server2 : Не пингуется!");
-                }
-                if (reachableServer3) {
-                    System.out.println("Запрос :" + i + " результат Server3 : Пингуется!");
-                } else {
-                    System.out.println("Запрос :" + i + "результат Server3 : Не пингуется!");
-                }
-                if (reachableServer4) {
-                    System.out.println("Запрос :" + i + " результат Server4 : Пингуется!");
-                } else {
-                    System.out.println("Запрос :" + i + "результат Server4 : Не пингуется!");
-                }
 
-                TimeUnit.SECONDS.sleep(1);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    //            TimeUnit.SECONDS.sleep(1);
+    //        } catch (Exception e) {
+    //            e.printStackTrace();
+    //        }
+    //    }
+        FileIni fileIni = new FileIni(args[0]); //Получаем полный путь к файлу настроек
+        System.out.println(fileIni.adresCount);
+        System.out.println(fileIni.adress);
+        System.out.println((fileIni.timeOut));
+        System.out.println((fileIni.time));
+        //fileIni.fileIni();
+        //ArrayList<String> adressList = fileIni.getAdressList();
     }
 
 }
